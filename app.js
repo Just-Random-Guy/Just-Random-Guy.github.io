d4773e5591cc853b102334e81674208f3521451c
Nazerhack79 <nazaranimated@gmail.com>
2023-04-04 22:10:11 +0500

    Not compelete
    Сделаны дизайны на мобильные устройства, планшеты, добавлены новые функции на обновление при изменений экрана сделаны кнопки на планшетные устройства

--- app.js
+++ app.js
@@ -1,62 +1,90 @@
 let scet=0;
 let i =0;
 let b =2;
-let positionOnclick = undefined;
-let positionMove = undefined;
-let positionDiff = undefined;
-let endPosition = 0;
-let classes=['firstScroll','secondBlck','thirdBlck','secondScroll']
-if(window.innerWidth<=760&&window.innerHeight<=600){
-document.querySelectorAll('.saidBlck')[1].setAttribute('style','left:150%;')
-document.querySelectorAll('.saidBlck')[2].setAttribute('style','left:150%;')
-document.querySelectorAll('.saidBlck')[3].setAttribute('style','left:150%;')
-document.getElementById('secondScroll').setAttribute('style','left:120%;')
-document.getElementById('firstScroll').setAttribute('style','left:-5%;')
-}
-if(window.outerWidth<=770&&window.outerHeight<=1000){
-    console.log(55)
-    document.querySelector('.menuBlck').setAttribute('style','margin-left: 150%;')
-}
-function a(a){
-    positionOnclick = a.clientX;
-}
-function bet(b) {
-    if(positionOnclick!=undefined&&document.querySelector('.SlideBlck').getAttribute){
-    positionMove = b.clientX;
-    positionDiff = positionOnclick-positionMove;
-    if(endPosition-positionDiff/document.querySelector('.SlideBlck').clientWidth*100<=10.5&&endPosition-positionDiff/document.querySelector('.SlideBlck').clientWidth*100>=-49.5)
-        document.querySelector('.SlideBlck').setAttribute('style',`margin-left: calc(${endPosition}% - (${positionDiff/document.querySelector('.SlideBlck').clientWidth*100}%));`)}
+function Screenresize() {
+    console.log(window.innerHeight,window.innerWidth);
+    if (document.body.clientHeight<=950&&document.body.clientWidth<=450){
+        window.location = './mobile.html'
+    }if (document.body.clientHeight>950&&document.body.clientWidth>450){
+        window.location = './index.html'
     }
-function c() {
-    if(positionDiff!=undefined){
-    positionOnclick=undefined;
-    endPosition -= positionDiff/document.querySelector('.SlideBlck').clientWidth*100
-    if(endPosition< -49.5){
-        endPosition=-49.5
+   }
+   function Scroll(a) {
+    let c = 'PC'; 
+    let d = 48.5
+    if(document.body.clientHeight<=1400&&document.body.clientWidth<=1000){
+        console.log(655);
+        c = 'Lap'
+        d = 63.5
     }
-    if(endPosition>10.5){
-        endPosition=10.5
-    }
-    console.log(endPosition)
-    positionDiff= undefined;
-    document.querySelector('.SlideBlck').setAttribute('style',`margin-left: ${endPosition}%;`)
-}}
-// Number(document.querySelector('.SlideBlck').getAttribute('style').split(': ')[1].split('%;')[0])
-function mobile_Menu(a) {
     if(a==1){
-        document.querySelector('[onclick="mobile_Menu(1)"]').setAttribute('hidden','')
-        document.querySelector('[onclick="mobile_Menu(0)"]').removeAttribute('hidden')
-        document.getElementById('mobile-nav').setAttribute('style','animation-name:mobilemenu;animation-duration:0.2s;margin-left:9%;')
+        document.querySelector('.SlideBlck').setAttribute('style',`animation-name:ScrollRight${c};animation-duration:0.4s;margin-left:-${d}%;`)
+        document.querySelector('[onclick="Scroll(0)"]').setAttribute('onclick','Scroll(2)')
+        document.querySelector('[onclick="Scroll(1)"]').setAttribute('onclick','Scroll(-1)')
     }
     if(a==0){
-        document.querySelector('[onclick="mobile_Menu(0)"]').setAttribute('hidden','')
-        document.querySelector('[onclick="mobile_Menu(1)"]').removeAttribute('hidden')
-        document.getElementById('mobile-nav').setAttribute('style','animation-name:mobilemenu2;animation-duration:0.2s;margin-left:150%;')  
+        document.querySelector('[onclick="Scroll(0)"]').setAttribute('onclick','Scroll(-1)')
+        document.querySelector('[onclick="Scroll(1)"]').setAttribute('onclick','Scroll(3)')
+        document.querySelector('.SlideBlck').setAttribute('style',`animation-name:ScrollLeftPC;animation-duration:0.4s;margin-left:10.5%;`)
+    }
+    if(a==2){
+        document.querySelector('[onclick="Scroll(2)"]').setAttribute('onclick','Scroll(0)')
+        document.querySelector('[onclick="Scroll(-1)"]').setAttribute('onclick','Scroll(1)')
+        document.querySelector('.SlideBlck').setAttribute('style',`animation-name:ScrollRight2${c};animation-duration:0.4s;margin-left:0%;`)
+    
+    }
+    if(a==3){ 
+        document.querySelector('[onclick="Scroll(3)"]').setAttribute('onclick','Scroll(1)')
+    document.querySelector('[onclick="Scroll(-1)"]').setAttribute('onclick','Scroll(0)')
+    document.querySelector('.SlideBlck').setAttribute('style',`animation-name:ScrollLeft2PC;animation-duration:0.4s;margin-left:0%;`)
+
+
+    }
+   }
+   function ScrollBtn(a) {
+    if(a==0){
+        document.querySelector('.SlideBlck').setAttribute('onmousemove','ScrollBtn(-1)')
+        document.querySelectorAll('[hidden]')[1].removeAttribute('hidden')
+        document.querySelectorAll('[hidden]')[1].removeAttribute('hidden')
+        document.querySelector('.leftScrollParent').setAttribute('style','animation-name:ScrollBtnVis;animation-duration:0.8s;background-color:rgba(0,0,0,0.4)')
+        document.querySelector('.rightScrollParent').setAttribute('style','animation-name:ScrollBtnVis;animation-duration:0.8s;background-color:rgba(0,0,0,0.4)')
+        document.querySelector('.leftScroll').setAttribute('style','animation-name:RightLeftScrollVis;animation-duration:0.2s;background-color:rgb(0,0,0)')
+        document.querySelector('.rightScroll').setAttribute('style','animation-name:ScrollBtnVis;animation-duration:0.2s;background-color:rgba(0,0,0)')
+        document.querySelector('.leftScroll2').setAttribute('style','animation-name:RightLeftScrollVis;animation-duration:0.2s;background-color:rgb(0,0,0)')
+        document.querySelector('.rightScroll2').setAttribute('style','animation-name:ScrollBtnVis;animation-duration:0.2s;background-color:rgba(0,0,0)')
+    
+    }
+    if(a==1){
+        document.querySelector('.SlideBlck').setAttribute('onmousemove','ScrollBtn(0)')
+        document.querySelector('.leftScrollParent').setAttribute('style','animation-name:ScrollBtnHid;animation-duration:0.8s;background-color:rgba(0,0,0,0.0)')
+        document.querySelector('.rightScrollParent').setAttribute('style','animation-name:ScrollBtnHid;animation-duration:0.8s;background-color:rgba(0,0,0,0.0)')
+        
+        document.querySelector('.leftScrollParent').setAttribute('hidden','')
+    document.querySelector('.rightScrollParent').setAttribute('hidden','')
+        document.querySelector('.leftScroll').setAttribute('style','animation-name:RightLeftScrollHid;animation-duration:0.2s;background-color:transparent')
+        document.querySelector('.rightScroll').setAttribute('style','animation-name:ScrollBtnHid;animation-duration:0.2s;background-color:transparent')
+        document.querySelector('.leftScroll2').setAttribute('style','animation-name:RightLeftScrollHid;animation-duration:0.2s;background-color:transparent')
+        document.querySelector('.rightScroll2').setAttribute('style','animation-name:ScrollBtnHid;animation-duration:0.2s;background-color:transparent')
+        
+
+
+    }
+   }
+let classes=['firstScroll','secondBlck','thirdBlck','secondScroll']
+function mobMenu(a) {
+    if(a==0){
+        document.querySelector('[onclick="mobMenu(0)"]').setAttribute('hidden','')
+        document.querySelector('[onclick="mobMenu(1)"]').removeAttribute('hidden')
+        document.getElementById('mobile-nav').setAttribute('style','visibility:visible;animation-name:mobilemenu;animation-duration:0.4s;margin-left:4%;')
+    }
+    if(a==1){
+        document.querySelector('[onclick="mobMenu(1)"]').setAttribute('hidden','')
+        document.querySelector('[onclick="mobMenu(0)"]').removeAttribute('hidden')
+        document.getElementById('mobile-nav').setAttribute('style','animation-name:mobilemenu2;animation-duration:0.4s;margin-left:104%;visibility:hidden;')  
     }
 }
 let emailService = ['mail.ru','gmail.com','outlook.com']
 function inbox(){
-    console.log(555)
     if(document.querySelector('[type="email"]').value.split('@')[0].length<6&&emailService.includes(document.querySelector('[type="email"]').value.split('@')[1])==false){
         document.querySelector('.footerInbox').children[3].removeAttribute('hidden')
         document.querySelector('.footerInbox').children[3].innerHTML="Please insert a valid email"
